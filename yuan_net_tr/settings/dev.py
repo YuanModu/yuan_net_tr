7a534fa4a2e01390af92bf8848647511259330d5
Atila Satilmis <atila.satilmis@gmail.com>
2023-06-27 11:43:21 +0300

    Add barebone Wagtail project files.

---
+++ yuan_net_tr/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-dcem_sc@9x0n&+d(k9955y6qo(-plaioya)1d+1^#sids$si8b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

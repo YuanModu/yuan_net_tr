f563b2e7d82cbba0c260a334dea88f43fb0f35e3
Atila Satilmis <atila.satilmis@gmail.com>
2023-06-27 20:13:19 +0300

    Update production.py

--- yuan_net_tr/settings/production.py
+++ yuan_net_tr/settings/production.py
@@ -2,6 +2,24 @@ from .base import *
 
 DEBUG = False
 
+# DJANGO_SECRET_KEY *should* be specified in the environment. If it's not, generate an ephemeral key.
+if "DJANGO_SECRET_KEY" in os.environ:
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+else:
+    # Use if/else rather than a default value to avoid calculating this if we don't need it
+    print(  # noqa: T201
+        "WARNING: DJANGO_SECRET_KEY not found in os.environ. Generating ephemeral SECRET_KEY."
+    )
+    SECRET_KEY = "".join(
+        [random.SystemRandom().choice(string.printable) for i in range(50)]
+    )
+
+
+# Accept all hostnames, since we don't know in advance which hostname will be used for any given Heroku instance.
+# IMPORTANT: Set this to a real hostname when using this in production!
+# See https://docs.djangoproject.com/en/3.2/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
 try:
     from .local import *
 except ImportError:
